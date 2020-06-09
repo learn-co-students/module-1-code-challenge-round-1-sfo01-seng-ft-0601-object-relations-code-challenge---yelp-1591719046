f34f19ce4538594018b18448dd15172bf62c617e
candacecodes <candacehchoe@gmail.com>
2020-06-09 10:36:02 -0700

    Ycommit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,42 @@
 class Customer
   attr_accessor :given_name, :family_name
+  @@all = [] 
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self 
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all 
+    @@all 
+  end 
+
+  def restaurants 
+    Review.all.select {|review| review.customer == self}.uniq  
+  end 
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end 
+
+  def num_reviews
+    Review.all.count {|review| review.customer == self}
+  end 
+
+  # def self.find_by_name(given_name, family_name)
+  #    #@@all.given_name == given_name 
+  #    #@@all.family_name == family_name 
+  #    #use find to return the first value from customer.all
+  #    all.find |customer| {customer.given_name,family_name == self} 
+  # end 
+
+  def self.find_all_by_given_name(given_name)
+    all.select {|customers| customers.given_name == self} 
+  end 
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,23 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#Customer.new(given_name, family_name)
+#Review.new(customer, restaurant, rating)
+#Restaurant.new(name)
+
+customer_one = Customer.new("given_one", "family_one")
+customer_two = Customer.new("given_two", "family_two")
+customer_three = Customer.new("given_three", "family_three")
+
+restaurant_one = Restaurant.new("restaurant_one")
+restaurant_two = Restaurant.new("restaurant_two")
+restaurant_three = Restaurant.new("restaurant_three")
+
+review_one = Review.new(customer_one, restaurant_one, "rating_one")
+review_two = Review.new(customer_two, restaurant_two, "rating_two")
+review_three = Review.new(customer_three, restaurant_three, "rating_three")
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
