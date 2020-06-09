8e29043619e674390b41bced9d3a0c082c8b9063
haleighdalke <haleighdalke@gmail.com>
2020-06-09 10:32:23 -0700

    Haleigh Dalke Final

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,22 @@ class Restaurant
     @name = name
   end
 
+  def reviews 
+    Review.all.select{ |review| review.restaurant == self }
+  end
+
+  def customers
+    reviews.collect { |review| review.customer }
+  end
+
+  def average_star_rating
+    reviews_sum = reviews.collect { |review| review.rating }.sum
+    
+    # prevent code from breaking if no reviews were created for a restaurant
+    if reviews.count > 0
+      reviews_sum / reviews.count
+    else
+       nil
+    end
+  end
 end
