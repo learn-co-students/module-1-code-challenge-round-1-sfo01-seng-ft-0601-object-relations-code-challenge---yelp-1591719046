6b5d20670d190f600a748cc28f740bd0c12e56cf
iona-b <ionabrabender@gmail.com>
2020-06-09 10:47:05 -0700

    06.09.2020 Code Challenge Part 2

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,23 @@
 class Review
+
+    attr_reader :customer, :restaurant, :rating
+
+    @@all = []
+
+    # Review#initialize: Reviews should be initialized with a customer, restaurant, and a rating (a number)
+    # Review#rating returns the rating for a restaurant.
+    # Review#customer returns the customer object for that review, Once a review is created, should not be able to change the customer
+    # Review#restaurant returns the restaurant object for that given review Once a review is created, should not be able to change the restaurant
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all # Review.all returns all of the reviews
+        @@all
+    end
   
 end
