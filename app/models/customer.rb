1d0b61cf725ea90929d9e84ac6465e3f907494fa
keeks5456 <Akikogreen08@gmail.com>
2020-06-09 10:58:27 -0700

    code challenge 1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,45 @@
 class Customer
   attr_accessor :given_name, :family_name
-
+@@all = []
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants
+    Review.all.collect {|review| review.restaurant}.uniq
+  end
+
+  def add_review(restaurant, rating)
+    new_review = Review.new(self, resturant, rating)
+  end
+
+  def num_reviews
+    Review.all.map{|review| review.restaurant}.count
+  end
+
+  def self.find_by_name(name)
+    binding.pry
+        self.each do |name|
+      name == name.customer
+    end
+  end
+
+    def self.find_all_by_given_name(name)
+      Review.each do |name|
+        if name == self.given_name
+          self
+        end
+    end
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,26 @@
 class Restaurant
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all << self
+  end
+
+  def review
+    Review.all.colect {|review| review.self }
+  end
+
+  def customers
+    Review.all.collect {|review| review.customers}.uniq
+  end
+
+  def average_star_rating
+    average_rate = Review.all.map{|rate| rate.rating}
+    
+  end
+  binding.pry
 end
